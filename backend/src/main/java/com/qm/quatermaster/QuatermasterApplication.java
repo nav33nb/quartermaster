eb3bda1b8633b5206feb12b3d6ce046fd4efe98d
Gurcharan Singh <gurcharan0993@gmail.com>
2023-01-26 13:29:19 +0530

    Add Intial Backend Project

---
+++ backend/src/main/java/com/qm/quatermaster/QuatermasterApplication.java
@@ -0,0 +1,13 @@
+package com.qm.quatermaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuatermasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuatermasterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/qm/quatermaster/QuatermasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qm.quatermaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuatermasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
